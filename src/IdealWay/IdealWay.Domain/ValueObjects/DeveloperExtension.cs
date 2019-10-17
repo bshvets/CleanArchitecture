30a408856f40c60d0c0551670f995708c1a01c10
Bogdan Shvets <shvets.bogdan@gmail.com>
2019-10-17 14:00:10 +0300

    Added ValueObject example

---
+++ src/IdealWay/IdealWay.Domain/ValueObjects/DeveloperExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IdealWay.Domain.Entities;
+using IdealWay.Domain.Enums;
+
+namespace IdealWay.Domain.ValueObjects
+{
+    public static class DeveloperExtension
+    {
+        public static void MakeDeveloperGreat(this Developer developer)
+        {
+            developer.Level = AbilitonLevel.Architect;
+        }
+    }
+}
